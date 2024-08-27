c7d864a9e97c06b449dba0e1ebcc4a40070daee4
Thiago Sobral Alves <thiagosobral23@gmail.com>
2024-08-27 18:23:57 +0000

    install crispy forms and add a new forms.py file

--- healthbite/settings.py
+++ healthbite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms'
     'recipes',
 ]
 
@@ -60,6 +61,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
