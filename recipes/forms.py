d013f6d2639bb9c8f65cdd1019f0540623a94a2a
Thiago Sobral Alves <thiagosobral23@gmail.com>
2024-09-05 17:36:32 +0000

    update style.css to make title bold and forms.py to insert title placehold

--- recipes/forms.py
+++ recipes/forms.py
@@ -2,12 +2,18 @@ from .models import Comment, Recipe
 from django import forms
 from django_summernote.widgets import SummernoteWidget
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
 
 class RecipeForm(forms.ModelForm):
+    """ Create Recipe Form """
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget = forms.TextInput(attrs={"placeholder": 'title'})
+
     class Meta:
         model = Recipe
         fields = ['title', 
